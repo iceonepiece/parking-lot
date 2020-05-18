8764837f7c1895ca55e64da0dc9ac47f1b53f556
iceonepiece <iceonepiece@gmail.com>
2020-05-18 22:17:21 +0700

    add route {GET /parking-lots/:id/car-plate-numbers}

--- src/routes/parkingLots.js
+++ src/routes/parkingLots.js
@@ -1,10 +1,12 @@
 import createParkingLot from '../controllers/createParkingLot';
+import getCarPlateNumbers from '../controllers/getCarPlateNumbers';
 import getParkingLotStatus from '../controllers/getParkingLotStatus';
 import parkCar from '../controllers/parkCar';
 import leaveSlot from '../controllers/leaveSlot';
 
 export default (fastify, opts, next) => {
   fastify.get('/:id', getParkingLotStatus);
+  fastify.get('/:id/car-plate-numbers', getCarPlateNumbers);
 
   fastify.post('/', createParkingLot);
   fastify.post('/:id/park', parkCar);
